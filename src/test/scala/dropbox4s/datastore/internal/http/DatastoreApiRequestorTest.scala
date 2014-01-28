916a8c05c908ddcaa6ddf8388bc8cd52ba50cd1a
Shinsuke Abe <mao.instantlife@gmail.com>
2014-01-28 22:38:44 +0900

    implemeting url requiest generate.

--- src/test/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestorTest.scala
+++ src/test/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestorTest.scala
@@ -5,11 +5,20 @@ package dropbox4s.datastore.internal.http
  */
 
 import org.specs2.mutable._
+import dropbox4s.datastore.auth.AccessToken
 
 class DatastoreApiRequestorTest extends Specification {
   "get_or_request_url" should {
     "throw exception when both parameter is null" in {
       DatastoreApiRequestor.getOrCreateUrl(null, null) must throwA[IllegalArgumentException]
+    }
+
+    "throw exception when dsid parameter is empty string" in {
+      DatastoreApiRequestor.getOrCreateUrl("", AccessToken("test-token")) must throwA[IllegalArgumentException]
+    }
+
+    "url that is set post parameter dsid and authrization header" in {
+      val url = DatastoreApiRequestor.getOrCreateUrl("test-datastore", AccessToken("test-token"))
     }
   }
 }
