eff0438c6e0405e21e4b851246861e9e39f13949
Shinsuke Abe <mao.instantlife@gmail.com>
2014-02-01 20:15:59 +0900

    impelemtns list_datastores url generate

--- src/main/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestor.scala
+++ src/main/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestor.scala
@@ -7,10 +7,19 @@ import dispatch._
  * @author mao.instantlife at gmail.com
  */
 object DatastoreApiRequestor {
+  val baseUrl = host("api.dropbox.com").secure / "1" / "datastores"
+
   def getOrCreateUrl(dsid: String, token: AccessToken) = {
     require(Option(dsid).isDefined && !dsid.isEmpty && Option(token).isDefined)
-    host("api.dropbox.com").secure / "1" / "datastores" / "get_or_create_datastore" <<
-      Map("dsid" -> dsid) <:<
-      Map("Authorization" -> s"Bearer ${token.token}")
+
+    baseUrl / "get_or_create_datastore" << Map("dsid" -> dsid) <:< authHeader(token)
   }
+
+  def listDatastoresUrl(token: AccessToken) = {
+    require(Option(token).isDefined)
+
+    baseUrl / "list_datastores" <:< authHeader(token)
+  }
+
+  private def authHeader(token: AccessToken) = Map("Authorization" -> s"Bearer ${token.token}")
 }
--- src/test/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestorTest.scala
+++ src/test/scala/dropbox4s/datastore/internal/http/DatastoreApiRequestorTest.scala
@@ -9,23 +9,47 @@ import dropbox4s.datastore.auth.AccessToken
 import dispatch._, Defaults._
 
 class DatastoreApiRequestorTest extends Specification {
+  val baseUrl = "https://api.dropbox.com/1/datastores"
+
+  val testToken = AccessToken("test-token")
+
+  def authHeaderValue(token: AccessToken) = s"Bearer ${token.token}"
+
+  implicit class RichReq(req: Req) {
+    def isDatastoresApi(endpoints: String, method: String, token: AccessToken) = {
+      req.url must equalTo(s"${baseUrl}${endpoints}")
+      req.toRequest.getMethod must equalTo(method)
+      req.toRequest.getHeaders.get("Authorization").get(0) must equalTo(authHeaderValue(token))
+    }
+  }
+
   "get_or_request_url" should {
     "throw exception when both parameter is null" in {
       DatastoreApiRequestor.getOrCreateUrl(null, null) must throwA[IllegalArgumentException]
     }
 
     "throw exception when dsid parameter is empty string" in {
-      DatastoreApiRequestor.getOrCreateUrl("", AccessToken("test-token")) must throwA[IllegalArgumentException]
+      DatastoreApiRequestor.getOrCreateUrl("", testToken) must throwA[IllegalArgumentException]
     }
 
-    "url that is set post parameter dsid and authrization header" in {
-      val req = DatastoreApiRequestor.getOrCreateUrl("test-datastore", AccessToken("test-token"))
+    "url that is set post parameter dsid and authorization header" in {
+      val req = DatastoreApiRequestor.getOrCreateUrl("test-datastore", testToken)
 
-      req.url must equalTo("https://api.dropbox.com/1/datastores/get_or_create_datastore")
-      req.toRequest.getMethod must equalTo("POST")
+      req isDatastoresApi ("/get_or_create_datastore", "POST", testToken)
       req.toRequest.getParams.size() must equalTo(1)
       req.toRequest.getParams.get("dsid").get(0) must equalTo("test-datastore")
-      req.toRequest.getHeaders.get("Authorization").get(0) must equalTo("Bearer test-token")
+    }
+  }
+
+  "list_datastores_url" should {
+    "throw exception when access token is null" in {
+      DatastoreApiRequestor.listDatastoresUrl(null) must throwA[IllegalArgumentException]
+    }
+
+    "url that is set authorization header" in {
+      val req = DatastoreApiRequestor.listDatastoresUrl(testToken)
+
+      req isDatastoresApi ("/list_datastores", "GET", testToken)
     }
   }
 }
