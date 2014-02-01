e5868f6bf2fc084f3c0ac3fe5267d578a2236a91
Shinsuke Abe <mao.instantlife@gmail.com>
2014-02-01 19:20:10 +0900

    test for list_datastores_result with ds_dict

--- src/test/scala/dropbox4s/datastore/internal/JsonConversionTest.scala
+++ src/test/scala/dropbox4s/datastore/internal/JsonConversionTest.scala
@@ -7,7 +7,12 @@ package dropbox4s.datastore.internal
 import org.specs2.mutable._
 import org.json4s._
 import org.json4s.native.JsonMethods._
-import dropbox4s.datastore.models.{DsInfo, ListDatastoresResult, GerOrCreateResult}
+import dropbox4s.datastore.models._
+import dropbox4s.datastore.models.WrappedTimestamp
+import scala.Some
+import dropbox4s.datastore.models.DsInfo
+import dropbox4s.datastore.models.ListDatastoresResult
+import dropbox4s.datastore.models.GerOrCreateResult
 
 class JsonConversionTest extends Specification {
   implicit val format = DefaultFormats
@@ -21,7 +26,7 @@ class JsonConversionTest extends Specification {
   }
 
   "list_datastores_result" should {
-    "json convert to ListDatastoresResult" in {
+    "json convert to ListDatastoresResult without InfoDict" in {
        val testResult = parse(
          """
            |{"datastores": [
@@ -34,6 +39,27 @@ class JsonConversionTest extends Specification {
           ListDatastoresResult(
             List(
               DsInfo("default", "1PuUJ3DvMI71OYx1gcqWHzzdva2EpF", 0)),
+            "cbd8804428bc888c7262b0193b43407033eb206b3e37bad2cc140591af3ec6f5"))
+    }
+
+    "json convert to ListDatastoresResult with InfoDict" in {
+      val testResult = parse(
+        """
+          |{"datastores": [
+          |   {
+          |     "handle": "1PuUJ3DvMI71OYx1gcqWHzzdva2EpF",
+          |     "rev": 0,
+          |     "dsid": "default",
+          |     "info": {"title" : "test-title", "mtime": {"T" : "test-mtime"}}}],
+          | "token": "cbd8804428bc888c7262b0193b43407033eb206b3e37bad2cc140591af3ec6f5"}
+        """.stripMargin)
+
+      testResult.extract[ListDatastoresResult] must
+        equalTo(
+          ListDatastoresResult(
+            List(
+              DsInfo("default", "1PuUJ3DvMI71OYx1gcqWHzzdva2EpF", 0, Some(
+                InfoDict("test-title", WrappedTimestamp("test-mtime"))))),
             "cbd8804428bc888c7262b0193b43407033eb206b3e37bad2cc140591af3ec6f5"))
     }
   }
