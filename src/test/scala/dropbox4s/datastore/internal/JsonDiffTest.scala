38e1eefee167338d9a255faba84975e8e5d9a49e
Shinsuke Abe <mao.instantlife@gmail.com>
2014-02-09 17:36:24 +0900

    add specification test

--- src/test/scala/dropbox4s/datastore/internal/JsonDiffTest.scala
+++ src/test/scala/dropbox4s/datastore/internal/JsonDiffTest.scala
@@ -28,5 +28,21 @@ class JsonDiffTest extends Specification {
 
       (value1 diff value2) must equalTo(Diff(JNothing, JObject(List(("key2", JString("value2")))), JNothing))
     }
+
+    "add value to list" in {
+      (parse("""{"key1": ["value1", "value2"]}""") diff parse("""{"key1": ["value1", "value2", "value3"]}""")) must
+        equalTo(
+          Diff(JNothing, JObject(List(("key1", JArray(List(JString("value3")))))), JNothing))
+    }
+
+    "delete value from list" in {
+      val value1 = parse("""{"key1": ["value1", "value2", "value3"]}""")
+      val value2 = parse("""{"key1": ["value1", "value3"]}""")
+
+      // deleted value from value1 is value3
+      // change value2 to value3 on list => get index from value2 by "value3"
+      (value1 diff value2) must
+        equalTo(Diff(JObject(List(("key1", JString("value3")))), JNothing, JObject(List(("key1", JArray(List(JString("value3"))))))))
+    }
   }
 }
