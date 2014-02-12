a407eb41a057cf745ccb8fa4846818d203764e3c
Shinsuke Abe <mao.instantlife@gmail.com>
2014-02-12 22:53:22 +0900

    implements data row operations method by condition, this is not tested yet.

--- src/main/scala/dropbox4s/datastore/DatastoresApi.scala
+++ src/main/scala/dropbox4s/datastore/DatastoresApi.scala
@@ -51,18 +51,41 @@ object DatastoresApi {
   def listDatastores(implicit token: AccessToken) = ListDatastoresRequestor.request(token)
 
   implicit def listDatastoresToList(list: ListDatastoresResult) = list.datastores
-
   implicit def datastoresToGetOrCreateResult(ds: Datastore) = ds.result.getOrElse(nullGetOrCreateDsResult)
 
   implicit class RichDataStore(val ds: Datastore) {
+    /**
+     * delete datastore.
+     *
+     * @param token access token
+     * @return delete result
+     */
     def delete(implicit token: AccessToken) = DeleteDatastoreRequestor.request(token, ds.handle)
 
+    /**
+     * get snapshot data list.
+     *
+     * @param token access token
+     * @return Snapshot instance
+     */
     def snapshot(implicit token: AccessToken) = Snapshot(ds.handle, GetSnapshotRequestor.request(token, ds.handle))
   }
 
   implicit class RichDsInfo(val dsInfo: DsInfo) {
+    /**
+     * delete datastore.
+     *
+     * @param token access token
+     * @return delete result
+     */
     def delete(implicit token: AccessToken) = DeleteDatastoreRequestor.request(token, dsInfo.handle)
 
+    /**
+     * get snapshot data list.
+     *
+     * @param token access token
+     * @return Snapshot instance
+     */
     def snapshot(implicit token: AccessToken) = Snapshot(dsInfo.handle, GetSnapshotRequestor.request(token, dsInfo.handle))
   }
 
@@ -71,15 +94,78 @@ object DatastoresApi {
       PutDeltaRequestor.request(
         token, PutDeltaParameter(table.handle, table.rev, None, ops))
 
-    def insert(rows: TableRow[T]*)(implicit token: AccessToken) =
-      putDeltaRequest(
-        rows.toList.map(row => DataInsert(table.tid, row.rowid, table.converter(row.data))), token)
+    private def select(where: (TableRow[T]) => Boolean) = table.rows.filter(where)
 
+    private def rowUpdateOps(rowid: String, other: T) =
+      table.rowDiff(rowid, other).map(DataUpdate(table.tid, rowid, _))
+
+    /**
+     * insert table rows to datastore.
+     * Note: if row id of parameter is conflict, throw IllegalArgumentException.
+     *
+     * @param rows (variable parameter) rows array to insert.
+     * @param token access token
+     * @return put_delta result
+     */
+    def insert(rows: TableRow[T]*)(implicit token: AccessToken) = {
+      require(rows.size == rows.toList.map(_.rowid).distinct.size)
+
+      putDeltaRequest(rows.toList.map(row => DataInsert(table.tid, row.rowid, table.converter(row.data))), token)
+    }
+
+    /**
+     * delete rows from datastore.
+     * Note: if rowid of parameter is conflict, distinct array before execute.
+     *
+     * @param rowids (variable parameter) row id array to delete.
+     * @param token access token
+     * @return put_delta result
+     */
     def delete(rowids: String*)(implicit token: AccessToken) =
-      putDeltaRequest(rowids.toList.map(DataDelete(table.tid, _)), token)
+      putDeltaRequest(rowids.distinct.toList.map(DataDelete(table.tid, _)), token)
 
+    /**
+     * delete rows from datastore by condition.
+     *
+     * @param where delete condition
+     * @param token access token
+     * @return put_delta result
+     */
+    def delete(where: (TableRow[T]) => Boolean)(implicit token: AccessToken) =
+      putDeltaRequest(select(where).map(row => DataDelete(table.tid, row.rowid)), token)
+
+    /**
+     * delete all rows of table.
+     *
+     * @param token access token
+     * @return put_delta result
+     */
+    def truncate(implicit token: AccessToken) =
+      putDeltaRequest(table.rows.map(row => DataDelete(table.tid, row.rowid)), token)
+
+    /**
+     * update row by rowid
+     * @param rowid rowid to update
+     * @param other update row data
+     * @param token access token
+     * @return put_delta result
+     */
     def update(rowid: String, other: T)(implicit token: AccessToken) =
-      putDeltaRequest(table.rowDiff(rowid, other).map(DataUpdate(table.tid, rowid, _)), token)
+      putDeltaRequest(rowUpdateOps(rowid, other), token)
+
+    /**
+     * update rows by condition.
+     *
+     * @param set function for update data
+     * @param where update condition
+     * @param token access token
+     * @return put_delta result
+     */
+    def update(set: (T) => T)(where: (TableRow[T]) => Boolean)(implicit token: AccessToken) = {
+      val updateRows = select(where).map(row => row.copy(data = set(row.data)))
+
+      putDeltaRequest(updateRows.map(row => rowUpdateOps(row.rowid, row.data)).flatten.toList, token)
+    }
   }
 
   val nullGetOrCreateDsResult = GetOrCreateDatastoreResult(null, 0, false)
