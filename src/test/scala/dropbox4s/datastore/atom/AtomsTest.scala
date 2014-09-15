1c8a3f572c10cfe221c9659b43f11d2dc386bda1
Shinsuke Abe <mao.instantlife@gmail.com>
2014-09-15 20:07:24 +0900

    add atoms test for DatastoreAPI value type.

---
+++ src/test/scala/dropbox4s/datastore/atom/AtomsTest.scala
@@ -0,0 +1,24 @@
+package dropbox4s.datastore.atom
+
+/**
+ * @author mao.instantlife at gmail.com
+ */
+
+import org.specs2.mutable._
+
+class AtomsTest extends Specification {
+
+  "WrappedInt" should {
+    // TODO parse json value to type
+    // TODO parse type value to json
+  }
+
+  // WrappedSpecial
+  // WrappedTimestamp(implemented, but not satisfied specification)
+  // WrappedBytes
+  // Dbase64 encode utilities
+  // AtomsUtility
+  // Int -> WrappedInt
+  // Double -> WrapedInt
+  // Short -> WrappedInt
+}
