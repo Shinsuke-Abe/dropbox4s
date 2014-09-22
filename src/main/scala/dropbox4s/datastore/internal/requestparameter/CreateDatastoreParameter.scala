68d31208c3f163f44a20daa4573243669c64c5ea
Shinsuke Abe <mao.instantlife@gmail.com>
2014-09-23 08:10:12 +0900

    add create_datastore request parameter class.

---
+++ src/main/scala/dropbox4s/datastore/internal/requestparameter/CreateDatastoreParameter.scala
@@ -0,0 +1,14 @@
+package dropbox4s.datastore.internal.requestparameter
+
+import java.security.MessageDigest
+
+import org.apache.commons.codec.binary.Base64
+
+/**
+ * @author mao.instantlife at gmail.com
+ */
+case class CreateDatastoreParameter(key: String) {
+  val messageDigest = MessageDigest.getInstance("SHA-256")
+
+  def dsid = Base64.encodeBase64URLSafeString(messageDigest.digest(key.getBytes))
+}
